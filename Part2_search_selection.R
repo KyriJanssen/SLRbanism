31f89261ed494e7d7780d55358538f5632d0a59a
KyriJanssen <105053276+KyriJanssen@users.noreply.github.com>
2024-03-13 13:45:14 +0100

    Update_KJ Part2_search_selection.R
    Added part 2.2 --> combining tables and removing duplicated references.

--- Part2_search_selection.R
+++ Part2_search_selection.R
@@ -6,7 +6,85 @@ library(wosr)
 library(rscopus)
 
 # Section 2: Combine tables, deduplicate references and summarise (Kyri?) 
+#install.packages("revtools")
+#install.packages("here")
+#install.packages("tidyverse")
 library(revtools)
+library(here)
+library(tidyvertse)
+
+# Read the bib or ris file (depending on Shuyu's part - may not be necessary)
+scopus_data <- read_bibliography(here("Data", "scopus.bib"))
+wos_data <-read_bibliography(here("Data", "web_of_science.bib"))
+
+# save variable names of dataframes is object
+unique_vars_scopus <- colnames(scopus_data)
+unique_vars_wos <- colnames(wos_data)
+
+#identify which columns the scopus and wos dataframes have in common
+common_vars <- intersect(unique_vars_scopus, unique_vars_wos)
+print(common_vars)
+
+#identify which columns are unique for the scopus and wos references
+unique_vars_only_scopus <- setdiff(unique_vars_scopus, unique_vars_wos)
+print(unique_vars_only_scopus)
+
+unique_vars_only_wos <- setdiff(unique_vars_wos, unique_vars_scopus)
+print(unique_vars_only_wos)
+
+# Rename the column in the wos_data from "issue" to "number"
+colnames(wos_data)[colnames(wos_data) == "issue"] <- "number"
+
+#select only the variables: label, author, type, title, year, volume and number in the scopus dataframe
+scopus_selection <- scopus_data%>%
+  select(c("label",  "title", "year", "journal", "volume", "number"))
+
+#do the same for the wos dataframe
+wos_selection <- wos_data%>%
+  select(c("label", "title", "year", "journal", "volume", "number"))
+
+#have a look at the subsetted dataframes 
+head(scopus_selection)
+head(wos_selection)
+
+#to make sure that our variables in the scopus and wos dataframes are represented in a similar way, we can remove all punctuations and capital letters from relevant character variables
+
+# First create function called `preprocess` where
+preprocess <- function(text) {
+  text <- tolower(text) #all characters are transformed to lower-case
+  text <- gsub("[[:punct:]]", "", text) #all punctuations are removed from the characters
+  return(text)
+}
+
+#apply this function to the scopus_selection dataframe
+scopus_selection$journal <- sapply(scopus_selection$journal, preprocess)
+scopus_selection$title <- sapply(scopus_selection$title, preprocess)
+scopus_selection$label <- sapply(scopus_selection$label, preprocess)
+
+#examine the scopus_selection dataframe
+head(scopus_selection)
+
+#do the same with wos dataframe
+wos_selection$journal <- sapply(wos_selection$journal, preprocess)
+wos_selection$title <- sapply(wos_selection$title, preprocess)
+wos_selection$label <- sapply(wos_selection$label, preprocess)
+
+#examine the wos_selection dataframe
+head(wos_selection)
+
+#combine the wos dataframe with the scopus dataframe
+all_references <- rbind(wos_selection, scopus_selection)
+
+# locate and extract unique references
+check_duplicates <- find_duplicates(all_references)
+unique_references <- extract_unique_references(all_references, matches = check_duplicates)
+
+#compare the total number of references to the total number of unique references
+#count the number of rows in the all_references dataframe
+nrow(all_references)
+#do the same for the references_unique dataframe
+nrow(unique_references)
+
 
 # Section 3: Suggest new keywords (Clementine) 
 
